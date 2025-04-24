3b2199a9f74c377e5c48fef8fba88e17a2297295
AV <a.o.vaskov90@gmail.com>
2025-04-25 00:34:51 +0300

    maxHeight60

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -23,18 +23,19 @@ namespace Model.Runtime.Projectiles
             
             Pos = Vector2.Lerp(StartPoint, _target, t);
             
-            float localHeight = 0f;
             float totalDistance = _totalDistance;
 
             ///////////////////////////////////////
             // Insert you code here
             ///////////////////////////////////////
+            float maxHeight = totalDistance * 0.6f;
+            float localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
 
 
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
